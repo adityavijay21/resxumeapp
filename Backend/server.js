ab19310c2ba1bcf7c7e7ae373219fc21b5b44e77
Aditya Vijay <kiriotheo@gmail.com>
2024-07-29 10:48:23 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 const opportunitiesRouter = require('./routes/opportunities');
 const resxumewritersRouter = require('./routes/resxumewriters');
 const slidersRouter = require('./routes/sliders');
-const resxumetemplateRouter = require('./routes/resxumetemplates');
-const dashboardRouter = require('./routes/dashboardRoutes');
-
+const resxumetemplateRouter = require ('./routes/resxumetemplates');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,9 +14,6 @@ app.use('/api/opportunities', opportunitiesRouter);
 app.use('/api/resxumewriters', resxumewritersRouter);
 app.use('/api/sliders', slidersRouter);
 app.use('/api/resxumetemplates', resxumetemplateRouter);
-app.use('/api/dashboard', dashboardRouter);
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log('changed');
 });
